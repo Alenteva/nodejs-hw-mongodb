afdb8157b0065d1f5b7bf04647f4e350932d5376
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-06-07 01:14:28 +0300

    ///

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,11 +6,12 @@ import {
   updateContact,
 } from '..//services/contacts.js';
 import createHttpError from 'http-errors';
-import { notFoundHandler } from '../middlewares/errorHandler.js';
+import { notFoundHandler } from '../middlewares/notFoundHandler.js';
 
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
   res.status(200).json({
+    status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
   });
@@ -26,6 +27,7 @@ export const getContactByIdController = async (req, res, next) => {
     return;
   }
   res.status(200).json({
+    status: 200,
     message: `Successfully found contact with id ${id}!`,
     data: contact,
   });
@@ -51,7 +53,7 @@ export const patchContactController = async (req, res, next) => {
   }
   res.json({
     status: 200,
-    message: `Successfully patched a student!`,
+    message: `Successfully patched a contact!`,
     data: result.contact,
   });
 };
