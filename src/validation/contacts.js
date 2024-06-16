17cc627b2dd7e2d45b7a2377444f180e84882dac
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-06-16 23:00:28 +0300

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,9 +1,17 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
+  name: Joi.string()
+    .min(3)
+    .max(20)
+    .required()
+    .messages({ 'string.base': 'Username should be a string' }),
   email: Joi.string().email().required(),
-  phoneNumber: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string()
+    .min(3)
+    .max(20)
+    .required()
+    .messages({ 'string.base': 'PhoneNumber should be a string' }),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
