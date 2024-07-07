1ec7b556bf5ac879071ccfe27e1ffb4200717d29
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-07-07 12:00:02 +0300

    ///

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -24,8 +24,8 @@ router.get('/:id', ctrlWrapper(getContactByIdController));
 
 router.post(
   '',
-  validateBody(createContactSchema),
   upload.single('photo'),
+  validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
@@ -33,8 +33,8 @@ router.delete('/:id', ctrlWrapper(deleteContactController));
 
 router.patch(
   '/:id',
-  validateBody(updateContactSchema),
   upload.single('photo'),
+  validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
 
